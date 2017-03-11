2d09a7c02b004afe1e24c56c6b3e9c05669874db
Francois Wautier <fw@fwconsult.com>
2017-03-11 10:28:29 +0700

    Removed debugging print

--- aiolifx/__init__.py
+++ aiolifx/__init__.py
@@ -1,9 +1,7 @@
 import sys
 if sys.version_info < (3, 5):
-    print("3.4")
     from .aiolifx34 import LifxDiscovery
 else:
-    print("3.5")
     from .aiolifx import LifxDiscovery
 from .message import *
 from .msgtypes import *
