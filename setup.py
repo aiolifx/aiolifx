fb6a0c66cd021f9c52b7900052dd0e501a0118a8
Francois Wautier <francois@wautier.eu>
2017-07-07 19:59:52 +0700

    Rev 0.5.2 with message size fix

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(name='aiolifx',
     packages=['aiolifx'],
-    version='0.5.1',
+    version='0.5.2',
     author='François Wautier',
     author_email='francois@wautier.eu',
     description='API for local communication with LIFX devices over a LAN with asyncio.',
     url='http://github.com/frawau/aiolifx',
-    download_url='https://github.com/frawau/aiolifx/archive/0.5.1.tar.gz',  
+    download_url='https://github.com/frawau/aiolifx/archive/0.5.2.tar.gz',  
     keywords = ['lifx', 'light', 'automation'], 
     license='MIT',
     install_requires=[
